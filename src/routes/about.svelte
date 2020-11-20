ebdf3a99e4ff9422120bbe6a475630d383341e1d
David Reynoso <rjsd13@gmail.com>
2020-11-20 12:04:03 -0600

    Este es el primer commit de mi blog

---
+++ src/routes/about.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<div class="About">
+	<h1>About me</h1>
+	<p>Hello, I'm Jesús Reynoso or as some may call me 'Chucho'. I'm 26 years old and I am a last-year medical student.
+		Since I was a teenager I was interested in technology and innovation.  </p>
+</div>
---
+++ src/routes/general.svelte
@@ -0,0 +1,39 @@
+<script context="module">
+	export function preload() {
+		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
+<script>
+  import Post from '../components/Post.svelte';
+  export let posts;
+  const filterPost = posts.filter(({primary_tag}) => primary_tag && primary_tag.name === 'Machine Learning' || 'Getting Started')
+</script>
+
+<style>
+  .Posts {
+    display:grid;
+    justify-content: space-between;
+    grid-gap: 30px;
+    grid-template-columns: 1fr;
+  }
+</style>
+
+<svelte:head>
+  <title>General</title>
+</svelte:head>
+
+<div class="General">
+  <h1>General</h1>
+  <div class="Posts">
+    {#if filterPost.length >= 1}
+      {#each filterPost as post}
+        <Post {post} />
+      {/each}
+    {:else}
+      <p>Sin resultados...</p>
+    {/if}
+  </div>
+</div>
