cdced930313303036aa69f8674594972c499fabe
David Reynoso <rjsd13@gmail.com>
2020-11-20 18:55:56 -0600

    SEO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,6 +22,24 @@
 
 <svelte:head>
 	<title>Current trends in health</title>
+	<meta name="description" content="Blog de Jesus Reynoso" />
+	
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:side" content="@jesusreynoso_"/>
+	<meta name="twitter:creator" content="@jesusreynoso_" />
+	<meta name="twitter:title" content="FuturoMedico" />
+	<meta name="twitter:description" content="Blog de Innovación en Medicina" />
+	<meta name="twitter:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+
+	<meta name="og:title" content="FuturoMedico" />
+	<meta name="og:description" content="Blog de Innovación en Medicina" />
+	<meta name="og:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+	<meta name="og:url" content="https://futuromedi.co" />
+	<meta name="og:site_name" content="FuturoMedico" />
+	<meta name="og:locale" content="es_ES" />
+	<meta name="og:type" content="article" />
+	<meta name="fb:app_id" content="" />
+
 </svelte:head>
 
 <div class="Home">
