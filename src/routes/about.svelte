c09003fb1d0f69a9f3ae47457f330e387a225efa
David Reynoso <rjsd13@gmail.com>
2020-11-20 19:05:32 -0600

    Agregue SEO a las demás pestañas

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,23 @@
 <svelte:head>
-	<title>About</title>
+	<title>Current trends in health: About</title>
+	<meta name="description" content="Blog de Jesus Reynoso" />
+	
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:side" content="@jesusreynoso_"/>
+	<meta name="twitter:creator" content="@jesusreynoso_" />
+	<meta name="twitter:title" content="FuturoMedico" />
+	<meta name="twitter:description" content="Blog de Innovación en Medicina" />
+	<meta name="twitter:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+
+	<meta name="og:title" content="FuturoMedico" />
+	<meta name="og:description" content="Blog de Innovación en Medicina" />
+	<meta name="og:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+	<meta name="og:url" content="https://futuromedi.co/about" />
+	<meta name="og:site_name" content="FuturoMedico" />
+	<meta name="og:locale" content="es_ES" />
+	<meta name="og:type" content="article" />
+	<meta name="fb:app_id" content="" />
+
 </svelte:head>
 
 <div class="About">
--- src/routes/general.svelte
+++ src/routes/general.svelte
@@ -22,7 +22,25 @@
 </style>
 
 <svelte:head>
-  <title>General</title>
+	<title>Current trends in health: General</title>
+	<meta name="description" content="Blog de Jesus Reynoso" />
+	
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:side" content="@jesusreynoso_"/>
+	<meta name="twitter:creator" content="@jesusreynoso_" />
+	<meta name="twitter:title" content="FuturoMedico" />
+	<meta name="twitter:description" content="Blog de Innovación en Medicina" />
+	<meta name="twitter:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+
+	<meta name="og:title" content="FuturoMedico" />
+	<meta name="og:description" content="Blog de Innovación en Medicina" />
+	<meta name="og:image" content="https://arepa.s3.amazonaws.com/blogpost-cover.png" />
+	<meta name="og:url" content="https://futuromedi.co/general" />
+	<meta name="og:site_name" content="FuturoMedico" />
+	<meta name="og:locale" content="es_ES" />
+	<meta name="og:type" content="article" />
+	<meta name="fb:app_id" content="" />
+
 </svelte:head>
 
 <div class="General">
